82b9f788e1f899398ffa4d41e84fb9c23ab64f87
Elias W <Elias.Wils@UGent.be>
2024-04-05 15:50:40 +0200

    clean notebook + istar small change

--- lib/istar_analysis.py
+++ lib/istar_analysis.py
@@ -32,35 +32,40 @@ def get_transition_probs(pes, w = None, tr=False):
         ensemble pe. The keys are the path types (RMR, RML, LMR, LML), and 
         the values are the local crossing probabilities.
     """
-    for pe in pes:
+
+
+    for i, pe in enumerate(pes):
         # Get the lmr masks, weights, ACCmask, and loadmask of the paths
         masks = get_lmr_masks(pe)
         if w is None:
-            w, _ = get_weights(pe.flags, ACCFLAGS, REJFLAGS, verbose = False)
+            w, ncycle_true = get_weights(pe.flags, ACCFLAGS, REJFLAGS, verbose = False)
+            assert ncycle_true == pe.ncycle
         accmask = get_flag_mask(pe, "ACC")
         loadmask = get_generation_mask(pe, "ld")
-        msg = f"Ensemble {pe.name} has {len(w)} paths.\n The total "+\
+        msg = f"Ensemble ...{pe.name[-10:]} has {len(w)} paths.\n The total "+\
                     f"weight of the ensemble is {np.sum(w)}\nThe total amount of "+\
                     f"accepted paths is {np.sum(accmask)}\nThe total amount of "+\
                     f"load paths is {np.sum(loadmask)}"
         logger.debug(msg)
 
+        # Get the weights of the RMR, RML, LMR and LML paths
+        w_path = {}
+        for pathtype in ["RMR", "RML", "LMR", "LML"]:
+            w_path[pathtype + str(i)] = np.sum(select_with_masks(w, [masks[pathtype],
+                                                            accmask, ~loadmask]))
+        msg = "Weights of the different paths:\n"+f"wRMR = {w_path['RMR'+ str(i)]}\n"+\
+                f"wRML = {w_path['RML'+ str(i)]}\nwLMR = {w_path['LMR'+ str(i)]}\n"+\
+                f"wLML = {w_path['LML'+ str(i)]}"
+        print(msg)
 
-    # Get the weights of the RMR, RML, LMR and LML paths
-    w_path = np.empty([2*pe.interfaces])
-    for pathtype in ["RMR", "RML", "LMR", "LML"]:
-        w_path[pathtype] = np.sum(select_with_masks(w, [masks[pathtype],
-                                                        accmask, ~loadmask]))
-    msg = "Weights of the different paths:\n"+f"wRMR = {w_path['RMR']}\n"+\
-            f"wRML = {w_path['RML']}\nwLMR = {w_path['LMR']}\n"+\
-            f"wLML = {w_path['LML']}"
-    print(msg)
-    if tr:  # TR reweighting. Note this is not block-friendly TODO
-        w_path['RMR'] = 2*w_path['RMR']
-        w_path['LML'] = 2*w_path['LML']
-        temp = w_path['RML'] + w_path['LMR']
-        w_path['RML'] = temp
-        w_path['LMR'] = temp
+        if tr:  # TR reweighting. Note this is not block-friendly TODO
+            w_path['RMR'+ str(i)] *= 2
+            w_path['LML'] *= 2
+            temp = w_path['RML'+ str(i)] + w_path['LMR'+ str(i)]
+            w_path['RML'+ str(i)] = temp
+            w_path['LMR'+ str(i)] = temp
+        
+
     # Get the total weight of paths starting from left, or from right
     wR = w_path['RMR'] + w_path['RML']
     wL = w_path['LMR'] + w_path['LML']
