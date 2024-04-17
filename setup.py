0310e3b7dd2252128d8fdd9646b4866d9b43625f
An Ghysels <annekegh@yahoo.co.uk>
2024-04-17 15:24:32 +0200

    convert to project that can be installed with pip

--- setup.py
+++ setup.py
@@ -3,24 +3,11 @@
 from glob import glob
 from distutils.core import setup
 
-setup(name='tistools',
+setup(
     version='0.1',
-    description='Tools to deal with the output of PyRETIS and RETIS projects',
-    author='An Ghysels',
-    #author_email='An.Ghysels@UGent.be',
-    #url='http://molmod.ugent.be/code/',
-    package_dir = {'tistools': 'lib'},
-    packages = ['tistools',],  #'mcdiff.tools','mcdiff.permeability'],
+    #package_dir = {'tistools': 'lib'},
+    #packages = ['tistools',],  #'mcdiff.tools','mcdiff.permeability'],
     scripts=glob("scripts/*"),
-    classifiers=[
-        #'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        #'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Topic :: Science/Engineering :: Molecular Science'
-    ],
 )
 
 
